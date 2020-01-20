2e2cf93f5161a16f5d29bed014741f2871368a43
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-20 12:06:15 +0700

    Tugas 19 Js

---
+++ tugas19.js
@@ -0,0 +1,11 @@
+function repeat() {
+  var isi1 = "Semangat...";
+  var isi2 = "****";
+
+  console.log(isi1.repeat(5));
+  console.log(isi2);
+  console.log(isi2.repeat(6));
+  console.log(isi2.repeat(12));
+  console.log(isi2.repeat(14));
+}
+repeat()
